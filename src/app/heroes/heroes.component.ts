8818dff5f016d73dfe3c1624e121de084da7403f
AARON-GR-99 <aarongarciar108@gmail.com>
2021-03-26 10:29:31 -0600

    Cambios con los formularios y el componente de heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero}from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: "Superman", edad: 30, poder: "Super Fuerza", identidad: "Clark Kent", universo: "DC"},
+    {id: 12, name: "Batman", edad: 30 , poder: "Ser Millonario", identidad: "Bruce Wayne", universo: "DC"},
+    {id: 13, name: "Hulk", edad: 40, poder: "Aumenta su Fuerza con la Ira", identidad: "Bruce Banner", universo: "Marval"},
+    {id: 14, name: "Ironman", edad: 25, poder: "Inteligencia (el poder esta en el traje)", identidad: "Tony Stark", universo: "Marval"},
+    {id: 15, name: "Thor", edad: 1000, poder: "Controlar los Rayos y es un dios", identidad: "Thor", universo: "Marval"},
+    {id: 16, name: "Spiderman", edad: 20, poder: "Puede Hacer Todo lo que una Araña y mas", identidad: "Piter Parker", universo: "Marval"},
+    {id: 17, name: "Black Widow", edad: 28, poder: "Combate Cuerpo a Cuerpo", identidad: "Natalia Alianovna", universo: "Marval"},
+    {id: 18, name: "Flash", edad: 28, poder: "Super Velocidad", identidad: "Barry Allen", universo: "DC"},
+    {id: 19, name: "Bruja Eacarlata", edad: 36, poder: "poderes magicos", identidad: "Wanda Maximoff", universo: "Marval"},
+    {id: 20, name: "Wonder Woman", edad: 130, poder: "Super fuerza y es una diosa", identidad: "Diana Prince", universo: "DC"}
+];
